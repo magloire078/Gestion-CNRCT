885f9c7934f2e34b71f8e8391e6553d476cd8bad
Your Name <magloire078@gmail.com>
2025-07-28 10:16:53 +0000

    Erreur FAILED_PRECONDITION: Please pass in the API key or set the GEMINI

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,7 +1,13 @@
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
+import { config } from 'dotenv';
+
+config({ path: `.env` });
+
 
 export const ai = genkit({
-  plugins: [googleAI()],
+  plugins: [googleAI({
+     apiKey: process.env.GEMINI_API_KEY || process.env.GOOGLE_API_KEY,
+  })],
   model: 'googleai/gemini-2.0-flash',
 });

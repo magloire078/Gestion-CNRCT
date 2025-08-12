f574bd582ff1a5fb49939df6358cfdf07ae98bec
Magloire Kouadio <magloire078@gmail.com>
2025-08-12 16:07:17 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

---
+++ src/next.config.ts
@@ -0,0 +1,29 @@
+require('dotenv').config();
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  reactStrictMode: false,
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: "https",
+        hostname: "firebasestorage.googleapis.com",
+      }
+    ],
+  },
+};
+
+export default nextConfig;

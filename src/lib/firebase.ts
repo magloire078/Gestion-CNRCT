9cd01820d470b14ceb75717710d1a819a2d62f83
Magloire Kouadio <magloire078@gmail.com>
2025-08-12 09:34:48 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -26,15 +26,17 @@ const storage = getStorage(app);
 
 // NOTE: The emulator connection is disabled to use live Firebase services
 // and resolve network issues in the development environment.
-// if (process.env.NODE_ENV === 'development') {
-//     try {
-//       connectAuthEmulator(auth, "http://127.0.0.1:9099", { disableWarnings: true });
-//       connectFirestoreEmulator(db, '127.0.0.1', 8080);
-//       connectStorageEmulator(storage, '127.0.0.1', 9199);
-//     } catch (e) {
-//       console.error("Could not connect to emulators", e);
-//     }
-// }
+/*
+if (process.env.NODE_ENV === 'development') {
+    try {
+      connectAuthEmulator(auth, "http://127.0.0.1:9099", { disableWarnings: true });
+      connectFirestoreEmulator(db, '127.0.0.1', 8080);
+      connectStorageEmulator(storage, '127.0.0.1', 9199);
+    } catch (e) {
+      console.error("Could not connect to emulators", e);
+    }
+}
+*/
 
 
 export { app, db, auth, storage };

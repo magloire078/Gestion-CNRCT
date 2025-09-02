37fda93573bde4a11393f1208572282c14dd3aca
Magloire Kouadio <magloire078@gmail.com>
2025-09-02 16:16:43 +0000

    pourquoi les images que je viens d'ajouter ne s'affichent pas

--- src/next.config.ts
+++ src/next.config.ts
@@ -21,6 +21,10 @@ const nextConfig: NextConfig = {
       {
         protocol: "https",
         hostname: "firebasestorage.googleapis.com",
+      },
+      {
+        protocol: "https",
+        hostname: "storage.googleapis.com",
       }
     ],
   },

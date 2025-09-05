7590b0074c525d8b092d9a182a23a00e67114e13
Magloire Kouadio <magloire078@gmail.com>
2025-09-05 16:49:12 +0000

    supprime le lien https qui s'affiche sur le pdf en bas des bulletins. su

--- src/app/payroll/[id]/page.tsx
+++ src/app/payroll/[id]/page.tsx
@@ -255,7 +255,6 @@ export default function PayslipPage() {
                     <div className="leading-tight">
                         <p className="font-bold">Chambre Nationale de Rois et Chefs Traditionnels (CNRCT)</p>
                         <p>Yamoussoukro, Riviera - BP 201 Yamoussoukro | Tél : (225) 30 64 06 60 | Fax : (+255) 30 64 06 63</p>
-                        <p>www.cnrct.ci - Email : info@cnrct.ci</p>
                     </div>
                 </footer>
             </div>

035b01dc3ffabff178beeaa5745931e46321c818
Magloire Kouadio <magloire078@gmail.com>
2025-09-16 12:21:20 +0000

    le Firebase project est gestion-cnrct et non gestion-cnrct-w8ljs

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -8,9 +8,9 @@ import { getAnalytics } from "firebase/analytics";
 const firebaseConfig: FirebaseOptions = {
   apiKey: "AIzaSyBuMgqk-I_mngDw4SYuNhOOLcF6JNchXhw",
   authDomain: "gestion-cnrct.firebaseapp.com",
-  databaseURL: "https://gestion-cnrct-default-rtdb.europe-west1.firebasedatabase.app",
+  databaseURL: "https://gestion-cnrct.firebaseio.com",
   projectId: "gestion-cnrct",
-  storageBucket: "gestion-cnrct.firebasestorage.app",
+  storageBucket: "gestion-cnrct.appspot.com",
   messagingSenderId: "126727792063",
   appId: "1:126727792063:web:55513c7e21531a87286d0a",
   measurementId: "G-TDXM581DZ5"

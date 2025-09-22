2a02384002416f97f4a9619abb832bd2f16c52ac
Magloire Kouadio <magloire078@gmail.com>
2025-09-22 00:28:17 +0000

    et si je remettais dans mon projet tout ce qui étais lié à gestion-cnrct

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -7,9 +7,9 @@ import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig: FirebaseOptions = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  authDomain: "gestion-cnrct-w8ljs.firebaseapp.com",
+  projectId: "gestion-cnrct-w8ljs",
+  storageBucket: "gestion-cnrct-w8ljs.appspot.com",
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,

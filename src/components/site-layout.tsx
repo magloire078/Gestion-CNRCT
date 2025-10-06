0ff378c1be9f372ee0663cf14927e0242e579fb4
Magloire Kouadio <magloire078@gmail.com>
2025-10-06 12:14:14 +0000

    essayons de grandir un peu la taille du logo à coté de Gestion CNRCT

--- src/components/site-layout.tsx
+++ src/components/site-layout.tsx
@@ -1,5 +1,4 @@
 
-
 "use client";
 
 import * as React from "react";
@@ -279,10 +278,10 @@ function AppLayout({ children }: { children: React.ReactNode }) {
     <SidebarProvider>
         <Sidebar>
           <SidebarHeader>
-            <div className="flex items-center gap-2">
-              <Avatar className="size-8 rounded-md">
+            <div className="flex items-center gap-3">
+              <Avatar className="size-10 rounded-md">
                 <AvatarImage src={settings?.mainLogoUrl} alt={settings?.organizationName} />
-                <AvatarFallback><Building2 className="size-5" /></AvatarFallback>
+                <AvatarFallback><Building2 className="size-6" /></AvatarFallback>
               </Avatar>
               <span className="text-lg font-semibold">
                 {settings?.organizationName}

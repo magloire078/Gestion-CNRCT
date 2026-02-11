e84819764a443bd346dff1f105f038d6a1d95565
Magloire Kouadio <magloire078@gmail.com>
2026-02-11 11:10:15 +0000

    les logos sur l'etiquete sont trop petite, augmente leur taille d'une fo

--- src/components/it-assets/print-single-label.tsx
+++ src/components/it-assets/print-single-label.tsx
@@ -28,16 +28,16 @@ export function PrintSingleLabel({ asset, settings, isPreview = false }: PrintSi
     >
       <div className="flex justify-between items-center w-full">
           {settings.mainLogoUrl ? (
-              <img src={settings.mainLogoUrl} alt="Logo principal" style={{ height: '6mm', width: 'auto', objectFit: 'contain' }} />
-          ) : <div style={{width: '6mm'}}></div>}
+              <img src={settings.mainLogoUrl} alt="Logo principal" style={{ height: '9mm', width: 'auto', objectFit: 'contain' }} />
+          ) : <div style={{width: '9mm'}}></div>}
           <div className="text-center leading-tight mx-1">
             <p className="font-bold" style={{ fontSize: '5.5pt' }}>Chambre Nationale des Rois</p>
             <p className="font-bold" style={{ fontSize: '5.5pt' }}>et Chefs Traditionnels</p>
             <p className="mt-0.5" style={{ fontSize: '5pt' }}>Tel : 27 30 64 06 60</p>
           </div>
           {settings.secondaryLogoUrl ? (
-              <img src={settings.secondaryLogoUrl} alt="Logo secondaire" style={{ height: '6mm', width: 'auto', objectFit: 'contain' }} />
-          ) : <div style={{width: '6mm'}}></div>}
+              <img src={settings.secondaryLogoUrl} alt="Logo secondaire" style={{ height: '9mm', width: 'auto', objectFit: 'contain' }} />
+          ) : <div style={{width: '9mm'}}></div>}
       </div>
 
       <div className="flex w-full items-center justify-between flex-grow mt-1">
